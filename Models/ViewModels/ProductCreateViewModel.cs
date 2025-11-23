9e54b824aecad37c9f10761655d762677c504270
Ensar Toraman <ensartrmn@gmail.com>
2025-11-23 15:35:41 +0300

    ssss

---
+++ Models/ViewModels/ProductCreateViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace SecondHandMarket.Models.ViewModels
+{
+    public class ProductCreateViewModel
+    {
+        [Required]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        public string Description { get; set; } = null!;
+
+        [Required]
+        [Range(0, 999999)]
+        public decimal Price { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        public IFormFile? ImageFile { get; set; }
+
+        // Dropdown için
+        public IEnumerable<Category>? Categories { get; set; }
+    }
+}
